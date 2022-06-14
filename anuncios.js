e574fa7bfc53933505cc7b522ee0f918bfde9c59
Guilherme Ramires <95935219+GuilhermeRamires2709@users.noreply.github.com>
2022-06-14 17:53:45 -0300

    Insert js file

---
+++ anuncios.js
@@ -0,0 +1,6 @@
+
+
+$titulo = document.getElementById('title');
+$descricao = document.getElementById('descricao');
+$preco = document.getElementById('preco');
+$dateVen = document.getElementById('dateValid');
--- formAnuncio.php
+++ formAnuncio.php
@@ -1,48 +1,51 @@
 <?php
 require_once "DB.php";
-require_once "anuncios.php"; 
-if(isset($_POST['entrar'])){
+require_once "anuncios.php";
+if (isset($_POST['entrar'])) {
     $novoAnuncio = new anuncio($_POST['title'], $_POST['descricao'], $_POST['preco'], $_POST['dateValid']);
-    $resultado=$novoAnuncio->inserir();
-    if($resultado){
+    $resultado = $novoAnuncio->inserir();
+    if ($resultado) {
         header("location: index.php");
-    }else{
+    } else {
         echo "Inserção não realizada";
     }
-}        
+}
 
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inserir Anuncios</title>
 </head>
+
+<body>
     <main class="container">
         <form actiom="anuncios.php" method="POST" enctype='multipart/form-data'>
             <div class="form-group">
                 <div class="form-group row">
-                <label for='title'>Título</label>
-                <?php echo "<input type='text' name='title' id='title'
+                    <label for='title'>Título</label>
+                    <?php echo "<input type='text' name='title' id='title'
                     placeholder='Insira aqui o título do seu anúncio'>" ?>
                 </div>
                 <div class="form-group row">
-                <label for='descricao'>Descrição</label>            
+                    <label for='descricao'>Descrição</label>
                     <?php echo "<input type='text' name='descricao' id='descricao'
                     placeholder='Descreva seu produto'>" ?>
-                    </div>
-                    <div class="form-group row">
-                    <label for='preco'>Preço</label>            
+                </div>
+                <div class="form-group row">
+                    <label for='preco'>Preço</label>
                     <?php echo "<input type='money-mask' name='preco' id='preco'
                     placeholder='R$'>" ?>
-                    </div>
-                    <div class="form-group row">
-                    <label for='data-de-validade'>Data de validade</label>                        
+                </div>
+                <div class="form-group row">
+                    <label for='data-de-validade'>Data de validade</label>
                     <?php echo "<input type='date' name='dateValid' id='dateValid'>" ?>
-                    </div>
+                </div>
             </div>
             <div class="footer">
                 <input type="submit" name="entrar" value="Entrar">
@@ -50,8 +53,6 @@ if(isset($_POST['entrar'])){
             </div>
         </form>
     </main>
-
-<body>
-    
+    <script type="text/javascript" src = "anuncios.js"> </script>
 </body>
 </html>
